bd3660bf8c955fe89f2259ee89d1d269b7ec4abc
shivammittal274 <mittal.shivam103@gmail.com>
2021-08-19 23:23:32 +0530

    Lip movement

--- lip_movement.py
+++ lip_movement.py
@@ -11,6 +11,23 @@ d_inner = [0]*3
 font = cv2.FONT_HERSHEY_SIMPLEX
 cap = cv2.VideoCapture(0)
 
+while(True):
+    ret, img = cap.read()
+    rects = find_faces(img, face_model)
+    for rect in rects:
+        shape = detect_marks(img, landmark_model, rect)
+        draw_marks(img, shape)
+        cv2.putText(img, 'Press r to record Mouth distances', (30, 30), font,
+                    1, (0, 255, 255), 2)
+        cv2.imshow("Output", img)
+    if cv2.waitKey(1) & 0xFF == ord('r'):
+        for i in range(100):
+            for i, (p1, p2) in enumerate(outer_points):
+                d_outer[i] += shape[p2][1] - shape[p1][1]
+            for i, (p1, p2) in enumerate(inner_points):
+                d_inner[i] += shape[p2][1] - shape[p1][1]
+        break
+cv2.destroyAllWindows()
 d_outer[:] = [x / 100 for x in d_outer]
 d_inner[:] = [x / 100 for x in d_inner]
 
